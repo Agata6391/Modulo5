124c1faaed811e2b23983945349f3998fdc674eb
Daniel VM <157084240+Agata6391@users.noreply.github.com>
2024-09-03 20:33:05 -0600

    inicio Clase 1 Mod5
    inicio Clase 1 Mod5

---
+++ clase1/src/App.js
@@ -0,0 +1,11 @@
+import LoginFields from './components/LoginFields';
+import './App.css';
+const App = ()=>{
+  return (
+    <div className="App">
+      <LoginFields/>
+    </div>
+  );
+};
+export default App;
+
---
+++ clase1/src/components/LoginFields.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import "./LoginFields.css"
+import Icon from "./Icon";
+
+
+const LoginFields = ()=>{
+    const initialValues = {
+        name: "",
+        email: "",
+    };
+
+    const validationSchema = Yup.object({
+        name: Yup.string().required("Required"),
+        email: Yup.string().required("Required"),
+    });
+
+    const onSubmit = (values)=>{
+        console.log("Submitted data:", values);
+    };
+
+    return(
+        <div className="main-container">
+            <div className="icon">
+                <Icon/>
+            </div>
+            <h2>Member Login</h2>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={onSubmit}
+            >
+                <Form>
+                    <div className="field-container">
+                        <label htmlFor="name">Nombre</label>
+                        <Field type="text" id="name" name="name"/>
+                        <ErrorMessage name="name" component="div" className="error-message"/>
+                    </div>
+                    <div className="field-container">
+                        <label htmlFor="email">Email</label>
+                        <Field type="text" id="email" name="email"/>
+                        <ErrorMessage name="email" component="div" className="error-message"/>
+                    </div>
+
+                    <button type="submit">Login</button>
+                </Form>
+            </Formik>
+        </div>
+    );
+};
+
+export default LoginFields;
